ac8d652f7da5f78b0d8482288dc343ae8f7676e3
kesavvvvv <kesavr1999@gmail.com>
2021-08-28 02:33:45 -0700

    add basic models and start code

---
+++ game.js
@@ -0,0 +1,45 @@
+kaboom({
+    global: true,
+    fullscreen: true,
+    scale: 1,
+    debug: true,
+    clearColor: [0, 0, 0, 1],
+})
+
+loadRoot('models/')
+loadSprite('coin', 'coin.png')
+loadSprite('monster', 'monster.png')
+loadSprite('brick', 'brick.png')
+loadSprite('block', 'block.png')
+loadSprite('mario', 'mario.png')
+loadSprite('mushroom', 'mushroom.png')
+loadSprite('pipe-top-left', 'block.png')
+loadSprite('pipe-top-right', 'brick.png')
+loadSprite('pipe-bottom-left', 'block.png')
+loadSprite('pipe-bottom-right', 'block.png')
+
+screen("game", () => {
+    layers(['bg', 'obj', 'ui'], 'obj')
+
+    const map = [
+        '                                                 ',
+        '                                                 ',
+        '                                                 ',
+        '                                                 ',
+        '                                                 ',
+        '                                                 ',
+        '                                                 ',
+        '                                                 ',
+        '===================================   ===========',
+    ]
+
+    const levelCfg = {
+        width: 20,
+        height: 20,
+        '=': [sprite('block', solid())]
+    }
+
+    const gameLevel = addLevel(map, levelCfg)
+})
+
+start("game")
