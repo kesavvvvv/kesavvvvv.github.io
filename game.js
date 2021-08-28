0e70034a502fa4998364b6e6c164d430927434a2
kesavvvvv <kesavr1999@gmail.com>
2021-08-28 11:41:35 -0700

    Add dirt and grass assets

--- game.js
+++ game.js
@@ -8,16 +8,28 @@ kaboom({
 
 
 
-// loadRoot('models/')
-// loadSprite('coin', 'https://i.imgur.com/vNCUro5.png');
-// loadSprite('monster', 'monster.png')
-// loadSprite('brick', 'brick.png')
-// loadSprite('block', 'https://i.imgur.com/FGd5ekd.jpg');
-loadSprite('block', 'https://i.imgur.com/oguSX26.jpg');
-loadSprite('player10', 'https://i.imgur.com/sCID4aw.png')
-loadSprite('player20', 'https://i.imgur.com/B2vGh5v.png')
-loadSprite('player30', 'https://i.imgur.com/sCID4aw.png')
-// loadSprite('player40', 'https://i.imgur.com/zSQq2y4.png')
+loadRoot('https://raw.githubusercontent.com/kesavvvvv/kesavvvvv.github.io/master/assets/')
+// // loadSprite('coin', 'https://i.imgur.com/vNCUro5.png');
+// // loadSprite('monster', 'monster.png')
+// // loadSprite('brick', 'brick.png')
+// // loadSprite('block', 'https://i.imgur.com/FGd5ekd.jpg');
+loadSprite('grass', 'grass.jpg');
+loadSprite('dirt', 'dirt.jpg');
+loadSprite('player10', 'models/player/run/run0.png')
+loadSprite('player20', 'models/player/run/run1.png')
+loadSprite('player30', 'models/player/run/run2.png')
+loadSprite('player40', 'models/player/run/run3.png')
+loadSprite('player50', 'models/player/run/run4.png')
+loadSprite('player60', 'models/player/run/run5.png')
+loadSprite('player70', 'models/player/run/run6.png')
+loadSprite('player80', 'models/player/run/run7.png')
+loadSprite('player90', 'models/player/run/run8.png')
+loadSprite('player100', 'models/player/run/run9.png')
+// loadSprite('player10', 'https://i.imgur.com/sCID4aw.png')
+// loadSprite('grass', 'https://i.imgur.com/iUKRS2B.png')
+// loadSprite('dirt', 'https://i.imgur.com/kfVKDva.png')
+
+
 
 // loadSprite('mushroom', 'mushroom.png')
 // loadSprite('pipe-top-left', 'block.png')
@@ -34,16 +46,28 @@ scene("game", () => {
         '                               ',
         '                               ',
         '                               ',
-        '                               ',
 
-        
-        '==================   =========='
+        '                               ',
+        '                               ',
+        '                               ',
+        '                               ',
+        '                               ', 
+        '==========   ==================',
+        '----------   ------------------'
     ]
 
     const levelCfg = {
-        width: 50,
-        height: 50,
-        '=': [sprite('block'), solid(), scale(2)]
+        width: 99,
+        height: 87,
+        '=': [
+            sprite('grass'),
+                
+        ],
+        '-': [
+            sprite('dirt'),
+            solid(),  
+        ]
+        
     }
 
     const gameLevel = addLevel(map, levelCfg)
@@ -61,7 +85,7 @@ scene("game", () => {
         player.move(250, 0)
         if(i%10 == 0)
             player.changeSprite('player' + i)
-        if(i<40){
+        if(i<100){
             i = i + 1
         }
         else {
