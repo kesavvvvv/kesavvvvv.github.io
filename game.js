1ed6047331dd9833ac9573a01475da7eba831751
kesavvvvv <kesavr1999@gmail.com>
2021-08-28 03:30:52 -0700

    should work now

--- game.js
+++ game.js
@@ -1,16 +1,16 @@
 kaboom({
     global: true,
     fullscreen: true,
-    scale: 1,
+    scale: 2,
     debug: true,
     clearColor: [0, 0, 0, 1],
-})
+});
 
 // loadRoot('models/')
-// loadSprite('coin', 'coin.png')
+loadSprite('coin', 'https://i.imgur.com/vNCUro5.png');
 // loadSprite('monster', 'monster.png')
- loadSprite('brick', 'https://i.imgur.com/AM4US5y.png')
-// loadSprite('block', 'block.png')
+// loadSprite('brick', 'brick.png')
+loadSprite('block', 'https://i.imgur.com/AM4US5y.png');
 // loadSprite('mario', 'mario.png')
 // loadSprite('mushroom', 'mushroom.png')
 // loadSprite('pipe-top-left', 'block.png')
@@ -18,28 +18,41 @@ kaboom({
 // loadSprite('pipe-bottom-left', 'block.png')
 // loadSprite('pipe-bottom-right', 'block.png')
 
-screen("game", () => {
+scene("game", () => {
     layers(['bg', 'obj', 'ui'], 'obj')
-
+    
+   
     const map = [
-        '                                                 ',
-        '                                                 ',
-        '                                                 ',
-        '                                                 ',
-        '                                                 ',
-        '                                                 ',
-        '                                                 ',
-        '                                                 ',
-        '===================================   ===========',
+        '                               ',
+        '                               ',
+        '                               ',
+        '                               ',
+        '                               ',
+        '                               ',
+        '                               ',
+        '==================   =========='
     ]
 
     const levelCfg = {
         width: 20,
         height: 20,
-        '=': [sprite('brick', solid())]
+        '=': [sprite('block', solid())]
     }
 
     const gameLevel = addLevel(map, levelCfg)
 })
 
-start("game")
+    // addLevel([
+    //     '    ',
+    //     '    ',
+    //     '    ',
+    //     '    ',
+    //     '    ',
+    //     'xxxx'
+    // ], {
+    //     width: 20,
+    //     height: 20,
+    //     'x': [sprite('block')]
+    // });
+
+go('game');
