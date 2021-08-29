3ff00bf5e4b35c8894b84c6562fd41ce5b8720cd
kesavvvvv <kesavr1999@gmail.com>
2021-08-29 10:51:00 -0700

    Use new kaboom version and testing assets in git

--- game.js
+++ game.js
@@ -8,8 +8,9 @@ kaboom({
 });
 
 //Setting root location to get sprite from github
-loadRoot('https://raw.githubusercontent.com/kesavvvvv/kesavvvvv.github.io/master/assets/')
+// loadRoot('https://raw.githubusercontent.com/kesavvvvv/kesavvvvv.github.io/master/assets/')
 
+loadRoot('assets/')
 //Load Sprites for base floor 
 loadSprite('grass', 'grass.png');
 loadSprite('dirt', 'dirt.png');
@@ -88,6 +89,25 @@ scene("game", () => {
     
     
 
+    
+           
+    
+    
+    var run = 1
+    // addButton("jump", vec2(100, 100), () => player.jump());
+    // addButton("right", vec2(100, 150), () => {
+    //     player.move(250, 0)
+    //     if(run%10 == 0)
+    //         player.changeSprite('run' + run)
+    //     if(run<100){
+    //         run = run + 1
+    //     }
+    //     else {
+    //         run = 1
+    //     }
+    // });
+    
+
     const map = [
         '                               ',
         '                               ',
@@ -112,6 +132,7 @@ scene("game", () => {
         '-': [
             sprite('dirt'),
             solid(),  
+            area()
         ]   
     }
 
@@ -121,24 +142,50 @@ scene("game", () => {
         sprite('run10'),
         pos(30,0),
         body(), 
+        area()
     ])
+
+    const bg1 = add([
+        pos(vec2(100, 100)),
+        rect(60, 30),
+        area(),
+        origin("center"),
+        color(1, 1, 1),
+        layer('ui')
+    ]);
+
+    const text1 = add([
+        text('jump', 8),
+        pos(vec2(100, 100)),
+        origin("center"),
+        color(0, 0, 0),
+        layer('ui')
+    ]);
+
+    bg1.action(() => {
+        bg1.moveTo(player.pos, 10)
+    })
+    // text1.action(() => {
+    //     text1.moveTo(player.pos, 10)
+    // })
 
     //Press q to go back to home screen
     keyPress('q', () => {
         go("home")
     })
 
-    // camera position follow player
-    player.action(() => {
-        camPos(player.pos);
-    });
+    // // camera position follow player
+    // player.action(() => {
+    //     camPos(player.pos);
+    // });
 
     var slide = 2
     keyDown("shift", () => {
         if (keyIsDown("right") || mouseIsDown()) {
             player.move(500, 0)
             if(slide%10 == 0)
-            player.changeSprite('slide' + slide)
+            player.use(sprite('slide' + slide))
+            // player.changeSprite('slide' + slide)
             if(slide<100){
                 slide = slide + 1
             }
@@ -150,28 +197,61 @@ scene("game", () => {
         }
     })
 
-    //Mobile 
-    //Swipe up on mobile for jump.
-    mouseClick(() => {  
+    // Mobile 
+    // Swipe up on mobile for jump.
+    // mouseClick(() => {  
+    //     yClick = mousePos().y
+    //     mouseRelease(() => {
+    //         yRelease = mousePos().y
+    //         if(yRelease < yClick - 350) {
+    //             player.jump()
+    //         }
+    //     })
+    // })
+
+    mouseClick(() => { 
+        console.log(mousePos())
+        if(mousePos().x>70 && mousePos().x<130 && mousePos().y>85 && mousePos().y<115) {
+            console.log(player.pos)
+            player.jump()
+        }
+    })
+
+    if(mouseIsClicked()) {
         yClick = mousePos().y
-        mouseRelease(() => {
+        console.log(yClick)
+        if(mouseIsReleased()) {
             yRelease = mousePos().y
+            console.log(yRelease)
             if(yRelease < yClick - 350) {
                 player.jump()
             }
-        })
-    })
+        }
+    }
+
+    // mouseClick(() => {  
+    //     // yClick = mousePos().y
+    //     console.log(mousePos().y)
+    //     mouseRelease(() => {
+    //         console.log(mousePos().y)
+    //         // yRelease = mousePos().y
+    //         // if(yRelease < yClick - 350) {
+    //         //     player.jump()
+    //         // }
+    //     })
+    // })
 
     var run = 2
     keyDown('right', () => {
         player.move(250, 0)
         if(run%10 == 0)
-            player.changeSprite('run' + run)
+            player.use(sprite('run' + run))
+            // player.changeSprite('run' + run)
         if(run<100){
             run = run + 1
         }
         else {
-            run=1
+            run = 1
         }
     }) 
 
@@ -179,15 +259,16 @@ scene("game", () => {
     mouseDown(() => {
         player.move(250, 0)
         if(run%10 == 0)
-            player.changeSprite('run' + run)
+            player.use(sprite('run' + run))
+            // player.changeSprite('run' + run)
         if(run<100){
             run = run + 1
         }
         else {
-            run=1
+            run = 1
         }
     })
 })
 
 //Start the website
-start('home');
+go('home');
