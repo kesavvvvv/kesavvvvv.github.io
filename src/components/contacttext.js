4cf47c0987e6ed968fd6c47ded5735b4b2d84030
kesavvvvv <kesavr1999@gmail.com>
2022-10-13 22:51:06 -0700

    text changes

--- src/components/contacttext.js
+++ src/components/contacttext.js
@@ -1,24 +1,16 @@
 export default function EduText() {
 
     add([
-        text("Hi! contact to edu page", {size: 30}), 
+        text("Email: keravich@ucsc.edu", {size: 30}), 
         origin('left'),
         layer('ui'),
         pos(250)
     ])
     add([
-        text("I am currently doing my Master of Science in Computer Science at UCSC.", {size: 30}), 
+        text("Phone Number: +1 (669) 837-5610", {size: 30}), 
         origin('left'),
         layer('ui'),
         // pos(1300, 250)
         pos(300)
-    ])
-
-    add([
-        text("Feel free to look around the different areas...", {size: 30}), 
-        origin('left'),
-        layer('ui'),
-        // pos(2500, 250)
-        pos(350)
     ])
 }
--- src/components/text.js
+++ src/components/text.js
@@ -2,13 +2,13 @@ export default function Text() {
 
     add([
         text("Hi! My name is Kesav Ravichandran. Welcome to my profile game.", {size: 30}), 
-        origin('center'),
+        origin('left'),
         layer('ui'),
         pos(250)
     ])
     add([
         text("Use the right and up arrow keys to move around using the keyboard.", {size: 30}), 
-        origin('center'),
+        origin('left'),
         layer('ui'),
         // pos(1300, 250)
         pos(300)
@@ -16,7 +16,7 @@ export default function Text() {
 
     add([
         text("Tap to move and swipe up to jump if you are on mobile.", {size: 30}), 
-        origin('center'),
+        origin('left'),
         layer('ui'),
         // pos(2500, 250)
         pos(350)
@@ -24,23 +24,55 @@ export default function Text() {
 
     add([
         text("I am currently pursuing my Master of Science in Computer Science at UCSC.", {size: 30}), 
-        origin('center'),
+        origin('left'),
         layer('ui'),
-        pos(1300, 250)
+        pos(1800, 250)
     ])
     add([
         text("I am a software developer engineer with 1.5 years of experience as a backend Java developer", {size: 30}), 
-        origin('center'),
+        origin('left'),
         layer('ui'),
         // pos(1300, 250)
-        pos(1300, 300)
+        pos(1800, 300)
     ])
 
     add([
         text("I am from Chennai, India.", {size: 30}), 
-        origin('center'),
+        origin('left'),
         layer('ui'),
         // pos(2500, 250)
-        pos(1300, 350)
+        pos(1800, 350)
+    ])
+
+    add([
+        text("About", {size: 30}), 
+        origin('left'),
+        layer('ui'),
+        // pos(2500, 250)
+        pos(2000, 350)
+    ])
+
+    add([
+        text("Projects", {size: 30}), 
+        origin('left'),
+        layer('ui'),
+        // pos(2500, 250)
+        pos(1800, 350)
+    ])
+
+    add([
+        text("Skills", {size: 30}), 
+        origin('left'),
+        layer('ui'),
+        // pos(2500, 250)
+        pos(1800, 350)
+    ])
+
+    add([
+        text("Contact", {size: 30}), 
+        origin('left'),
+        layer('ui'),
+        // pos(2500, 250)
+        pos(1800, 350)
     ])
 }
