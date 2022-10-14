fc64d2960852df634024b317a8a557e59bed0c3f
kesavvvvv <kesavr1999@gmail.com>
2022-10-13 21:18:27 -0700

    reduce font size

--- src/components/text.js
+++ src/components/text.js
@@ -1,20 +1,20 @@
 export default function Text() {
 
     add([
-        text("Hi! Welcome to my game.", {size: 46}), 
+        text("Hi! Welcome to my game.", {size: 30}), 
         origin('center'),
         layer('ui'),
         pos(250)
     ])
     add([
-        text("I am an engineer from Chennai, India.", {size: 46}), 
+        text("I am an engineer from Chennai, India.", {size: 30}), 
         origin('center'),
         layer('ui'),
         pos(1300, 250)
     ])
 
     add([
-        text("Feel free to look around the different areas...", {size: 46}), 
+        text("Feel free to look around the different areas...", {size: 30}), 
         origin('center'),
         layer('ui'),
         pos(2500, 250)
