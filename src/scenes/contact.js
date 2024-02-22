69ffb761b00457174d459269a047a338570beebf
Kesav Ravichandran <49778131+kesavvvvv@users.noreply.github.com>
2024-02-21 17:22:57 -0800

    Update contact.js

--- src/scenes/contact.js
+++ src/scenes/contact.js
@@ -66,7 +66,23 @@ export default function Contact() {
     //     })
 
     // }
-    
+
+    var yClick = 0
+    var yRelease = 0
+    mouseClick(() => {  
+        yClick = mousePos().y
+        console.log("inside mouse click")
+        console.log(yClick)
+        mouseRelease(() => {
+            yRelease = mousePos().y
+            console.log("inside mouse release")
+            console.log(mouseRelease)
+            if(yRelease < yClick - 350) {
+                console.log("inside jump")
+                player.jump()
+            }
+        })
+    })
 
     
 
