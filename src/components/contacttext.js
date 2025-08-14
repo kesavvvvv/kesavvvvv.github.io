728c360f88a2c178d51464efbd63088113b5dd07
Kesav Ravichandran <49778131+kesavvvvv@users.noreply.github.com>
2025-08-14 16:59:47 -0700

    Update contacttext.js

--- src/components/contacttext.js
+++ src/components/contacttext.js
@@ -6,13 +6,13 @@ export default function EduText() {
         layer('ui'),
         pos(250)
     ])
-    add([
-        text("Phone Number: +1 (669) 837-5610", {size: 30}), 
-        origin('left'),
-        layer('ui'),
-        // pos(1300, 250)
-        pos(300)
-    ])
+    // add([
+    //     text("Phone Number: +1 (669) 837-5610", {size: 30}), 
+    //     origin('left'),
+    //     layer('ui'),
+    //     // pos(1300, 250)
+    //     pos(300)
+    // ])
 
     add([
         text("GitHub", {size: 30}), 
